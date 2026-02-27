c05d1ef58406bdb50679d50319fcb110ecaa7c17
AlenaRaays <alenasepeleva575@gmail.com>
2026-02-27 14:14:55 +0300

    hh

--- CulnryBookUP/Data/ApplicationDbContext.cs
+++ CulnryBookUP/Data/ApplicationDbContext.cs
@@ -6,7 +6,8 @@ namespace CulnryBookUP.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
--- CulnryBookUP/Models/User.cs
+++ CulnryBookUP/Models/User.cs
@@ -7,12 +7,26 @@ namespace CulnryBookUP.Models
     public class User
     {
         [Key] public int IdUser { get; set; }
-        [Required (ErrorMessage = "Имя обязательно"), StringLength(50, MinimumLength = 3)] public string UserName { get; set; }
-        [Required(ErrorMessage = "Логин обязателен"), StringLength(50, MinimumLength = 3)] public string Login { get; set; }
-        [Required(ErrorMessage = "Пароль обязателен"), StringLength(50, MinimumLength = 8)] public string Password { get; set; }
-        [EmailAddress(ErrorMessage = "Некорректный email")] public string Email { get; set; }
-        [ForeignKey("IdRole")] public int IdRole { get; set; } = 1;
+
+        [Required (ErrorMessage = "Имя обязательно"), 
+            StringLength(50, MinimumLength = 3)] 
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Логин обязателен"), 
+            StringLength(50, MinimumLength = 3)] 
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен"), 
+            StringLength(50, MinimumLength = 8)] 
+        public string Password { get; set; }
+
+        [EmailAddress(ErrorMessage = "Некорректный email")] 
+        public string Email { get; set; }
+
+        [ForeignKey("IdRole")] 
+        public int IdRole { get; set; } = 1;
         public virtual Role Role { get; set; }
+
         public ICollection<Recipe> Recipes { get; set; }
         public User() { }
 
